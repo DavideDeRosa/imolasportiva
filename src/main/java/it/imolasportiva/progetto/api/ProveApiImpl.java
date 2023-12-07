b6732b5696df3c3d4b919d77b6562f8285f2a9c6
davidederosa <davide.derosa@studio.unibo.it>
2023-12-07 13:30:05 +0100

    modifica metodo provaFile(), permettendo il download di un file .ics di provapaths:

--- src/main/java/it/imolasportiva/progetto/api/ProveApiImpl.java
+++ src/main/java/it/imolasportiva/progetto/api/ProveApiImpl.java
@@ -3,23 +3,49 @@ package it.imolasportiva.progetto.api;
 import imolasportiva.api.ProveApi;
 import imolasportiva.model.TestDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Controller
 @RequestMapping("/api/v1")
 public class ProveApiImpl implements ProveApi {
 
-    public ResponseEntity<String> provaFile(String format){
+    public ResponseEntity<Object> provaFile(Object format){
         if("ics".equals(format)){
-            log.info("Invocazione provaFile() parte ics");
-            return new ResponseEntity<String>("ICS content", HttpStatus.OK);
+            try{
+                log.info("Invocazione provaFile() parte ics");
+
+                ClassPathResource icsFile = new ClassPathResource("icsFiles/invite.ics");
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.parseMediaType("text/calendar"));
+                headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=calendar.ics");
+
+                InputStream inputStream = icsFile.getInputStream();
+                InputStreamResource resource = new InputStreamResource(inputStream);
+
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .contentLength(headers.getContentLength())
+                        .body(resource);
+            }catch (IOException e){
+                log.info(e.getMessage());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
         }else{
             log.info("Invocazione provaFile() parte json");
-            return new ResponseEntity<String>("JSON content", HttpStatus.OK);
+
+            return ResponseEntity.ok("json richiamato");
         }
     }
 
